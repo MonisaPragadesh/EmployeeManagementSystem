8ae47a6b56669761c32efcaed7cae333f367b098
MonisaPragadesh <165965673+MonisaPragadesh@users.noreply.github.com>
2024-04-04 16:35:23 -0400

    added projects 2 and 3

---
+++ Employee Management System/employee_ms.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE employee;
+
+use employee;
+
+create table empdata (
+   Id INT,
+    NAME VARCHAR(60),
+    Email_Id VARCHAR(60),
+    Phone_no BIGINT, 
+    Address VARCHAR(60), 
+    Post VARCHAR(60), 
+    Salary int
+);
